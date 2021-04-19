6a4c998efe915ed8c37373bd5ee35a50def9d657
RAMI <ramid1703@gmail.com>
2021-04-19 20:24:13 +0300

    'test'

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 
 export function HomePage() {
+    const[num,setNum] = useState(5);
     return (
         <div className="home-page-container main-container">
-            HOME-PAGE!
+            HOME-PAGE! {num}
         </div>
     )
 }
